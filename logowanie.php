065e134672831af66f812e9436469d33b97fb825
Mariusz <mariusz.kolendo2004@gmail.com>
2025-05-17 00:22:33 +0200

    Zastąpiono pliki zawartością z mariusz

--- logowanie.php
+++ logowanie.php
@@ -1,53 +1,47 @@
 <?php
+session_start(); 
+
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "projekt_www";
 
-
 $conn = new mysqli($servername, $username, $password, $dbname);
-
-
 if ($conn->connect_error) {
     die("Błąd połączenia: " . $conn->connect_error);
 }
 
-
 $user = $_POST['lusername'];
 $pass = $_POST['lpassword'];
 
-
-$stmt = $conn->prepare("SELECT password FROM uzytkownicy WHERE username = ?");
+$stmt = $conn->prepare("SELECT id, password FROM uzytkownicy WHERE username = ?");
 $stmt->bind_param("s", $user); 
 $stmt->execute();
 $result = $stmt->get_result();
-
 
 if ($result->num_rows === 1) {
     $row = $result->fetch_assoc();
     $hash = $row['password'];
 
-    
     if (password_verify($pass, $hash)) {
-        
+        $_SESSION['user_id'] = $row['id']; 
+
         echo "<script>
                 alert('Udało się zalogować');
-                window.location.href = 'index.html'; // Przekierowanie na stronę główną
+                window.location.href = 'stronka.php'; 
               </script>";
     } else {
-        
         echo "<script>
                 alert('Błędne hasło');
-                window.location.href = 'index.html'; // Przekierowanie na stronę główną
+                window.location.href = 'index.html'; 
               </script>";
     }
 } else {
     echo "<script>
             alert('Nie znaleziono użytkownika');
-            window.location.href = 'index.html'; // Przekierowanie na stronę główną
+            window.location.href = 'index.html'; 
           </script>";
 }
-
 
 $stmt->close();
 $conn->close();
