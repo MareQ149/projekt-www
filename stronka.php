e9953f7dea30ffe56d7550fde1a1bb717c4cb7fc
Lukasz Perkowski <lperkowski05@gmail.com>
2025-05-22 01:13:16 +0200

    poprawki

--- stronka.php
+++ stronka.php
@@ -200,7 +200,7 @@ foreach ($all_slots as $slot => $item) {
             <li><a href="stronka.php">Profil</a></li>
             <li><a href="zbrojmistrz.php">Zbrojmistrz</a></li>
             <li><a href="kowal.php">Kowal</a></li>
-            <li><a href="walka.html">Walka</a></li>
+            <li><a href="walka.php">Walka</a></li>
             <li><a href="logout.php">Wyloguj</a></li>
         </ul>
     </div>
--- walka.php
+++ walka.php
@@ -138,8 +138,8 @@ $conn->close();
     <div id="dropdownMenu" class="hidden">
         <ul>
             <li><a href="stronka.php">Profil</a></li>
-            <li><a href="zbrojmistrz.html">Zbrojmistrz</a></li>
-            <li><a href="kowal.html">Kowal</a></li>
+            <li><a href="zbrojmistrz.php">Zbrojmistrz</a></li>
+            <li><a href="kowal.php">Kowal</a></li>
             <li><a href="walka.php">Walka</a></li>
             <li><a href="logout.php">Wyloguj</a></li>
         </ul>
